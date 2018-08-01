e0f9392e1851a1ef3333052c43a4ba5b36c1c6a4
wilson0x4d <mrshaunwilson@msn.com>
2018-07-31 19:35:25 -0700

    [collections] add `IIndex<TKey,TValue>` interface, and add new `IBag<TKey,TValue>` variant

--- algs4net/Collections/IBag.cs
+++ algs4net/Collections/IBag.cs
@@ -2,9 +2,25 @@
 
 namespace algs4net.Collections
 {
+    /// <summary>
+    /// Represents a bag of <typeparamref name="T"/>.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
     public interface IBag<T>
         where T : IComparable<T>
     {
         void Add(T item);
+    }
+
+    /// <summary>
+    /// Represents a bag of <typeparamref name="TKey"/> and <typeparamref name="TValue"/> pairs.
+    /// name="TValue"/> pairs.
+    /// </summary>
+    /// <typeparam name="TKey"></typeparam>
+    /// <typeparam name="TValue"></typeparam>
+    public interface IBag<TKey, TValue>
+        where TKey : IComparable<TKey>
+    {
+        void Add(TKey key, TValue value);
     }
 }
---
+++ algs4net/Collections/IIndex.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace algs4net.Collections
+{
+    /// <summary>
+    /// An interface for implementing indexed data (ie. Symnbol
+    /// Tables/Dictionaries/etc) where a key yields a value.
+    /// </summary>
+    /// <typeparam name="TKey"></typeparam>
+    /// <typeparam name="TValue"></typeparam>
+    /// <remarks>
+    /// Expect additional <![CDATA[IIndex<...>]]> interfaces.
+    ///
+    /// Iterating an Index yields all keys stored to the index, this behavior
+    /// differs from typical data structure implementations which also yield
+    /// a value component (this is an intentional design decision intentional.)
+    /// </remarks>
+    public interface IIndex<TKey, TValue> :
+        IEnumerable<TKey>
+        where TKey : IComparable<TKey>, IEquatable<TKey>
+    {
+        TValue this[TKey key] { get; set; }
+
+        bool TryGet(TKey key, out TValue value);
+
+        bool TryRemove(TKey key, out TValue value);
+    }
+}
