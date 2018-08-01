652e0406dd9ed089e5aca0bdfeaef286bd3307aa
wilson0x4d <mrshaunwilson@msn.com>
2018-07-31 20:53:55 -0700

    [collections] add `SequentialIndex` implementation (reference impl) and tests

---
+++ algs4net.Tests/Collections/SequentialIndexTests.cs
@@ -0,0 +1,145 @@
+﻿using algs4net.Collections;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Diagnostics;
+using System.Linq;
+
+namespace algs4net.Tests.Collections
+{
+    [TestClass]
+    public class SequentialIndexTests
+    {
+        [TestMethod]
+        public void SequentialIndex_Add_CanUpdateExistingEntry()
+        {
+            var guid = Guid.NewGuid();
+            var expectedSymbols = new[]
+            {
+                new { Key = guid, Value = Guid.NewGuid() },
+                new { Key = guid, Value = Guid.NewGuid() }
+            };
+            IIndex<Guid, Guid> index = new SequentialIndex<Guid, Guid>();
+            foreach (var expectedSymbol in expectedSymbols)
+            {
+                index[expectedSymbol.Key] = expectedSymbol.Value;
+            }
+            Assert.AreEqual(1, index.Count());
+            index.Trace();
+        }
+
+        [TestMethod]
+        public void SequentialIndex_GetEnumerator_YieldsExpectedSet()
+        {
+            var expectedKeys = Generators.IntegralNumberGenerator
+                .YieldPredictableSeries(10000)
+                .ToArray();
+            IIndex<int, Guid> index = new SequentialIndex<int, Guid>();
+            foreach (var key in expectedKeys)
+            {
+                index[key] = Guid.NewGuid();
+            }
+            expectedKeys = expectedKeys
+                .Distinct()
+                .OrderBy(e => e)
+                .ToArray();
+            Assert.AreEqual(expectedKeys.Length, index.Count());
+            int i = 0;
+            foreach (var actualKey in index)
+            {
+                Assert.AreEqual(expectedKeys[i], actualKey);
+                i++;
+            }
+            index.Trace();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void SequentialIndex_indexer_ThrowsOnMiss()
+        {
+            IIndex<Guid, Guid> index = new SequentialIndex<Guid, Guid>();
+            var result = index[Guid.NewGuid()];
+        }
+
+        [TestMethod]
+        public void SequentialIndex_indexer_YieldsExpectedSet()
+        {
+            var expectedKeys = Generators.IntegralNumberGenerator
+                .YieldPredictableSeries(10000)
+                .ToArray();
+            IIndex<int, int> index = new SequentialIndex<int, int>();
+            foreach (var key in expectedKeys)
+            {
+                index[key] = key ^ 0x4d;
+            }
+            expectedKeys = expectedKeys
+                .Distinct()
+                .ToArray();
+            Assert.AreEqual(expectedKeys.Length, index.Count());
+            foreach (var expectedKey in expectedKeys)
+            {
+                var actualValue = index[expectedKey];
+                Assert.AreEqual(expectedKey ^ 0x4d, actualValue);
+            }
+            index.Trace();
+        }
+
+        [TestMethod]
+        public void SequentialIndex_Keys_YieldsExpectedSet()
+        {
+            var expectedKeys = Generators.IntegralNumberGenerator
+                .YieldPredictableSeries(10000)
+                .ToArray();
+            IIndex<int, Guid> index = new SequentialIndex<int, Guid>();
+            foreach (var key in expectedKeys)
+            {
+                index[key] = Guid.NewGuid();
+            }
+            expectedKeys = expectedKeys
+                .Distinct()
+                .OrderBy(e => e)
+                .ToArray();
+            Assert.AreEqual(expectedKeys.Length, index.Count());
+            int i = 0;
+            foreach (var actualKey in index)
+            {
+                Assert.AreEqual(expectedKeys[i], actualKey);
+                i++;
+            }
+            index.Trace();
+        }
+
+        [TestMethod]
+        public void SequentialIndex_Remove_DoesNotThrowOnMiss()
+        {
+            IIndex<Guid, Guid> index = new SequentialIndex<Guid, Guid>();
+            Assert.IsFalse(index.TryRemove(Guid.NewGuid(), out Guid discard));
+            index.Trace();
+        }
+
+        [TestMethod]
+        public void SequentialIndex_Values_YieldsExpectedSet()
+        {
+            var expectedKeys = Generators.IntegralNumberGenerator
+                .YieldPredictableSeries(10000)
+                .ToArray();
+            IIndex<int, int> index = new SequentialIndex<int, int>();
+            foreach (var key in expectedKeys)
+            {
+                index[key] = key ^ 0x4d;
+            }
+            expectedKeys = expectedKeys
+                .Distinct()
+                .OrderBy(e => e)
+                .ToArray();
+            Assert.AreEqual(expectedKeys.Length, index.Count());
+            int i = 0;
+            foreach (var expectedKey in expectedKeys)
+            {
+                var actualValue = index[expectedKey];
+                Assert.AreEqual(expectedKey ^ 0x4d, actualValue);
+                i++;
+            }
+            index.Trace();
+        }
+    }
+}
---
+++ algs4net/Collections/SequentialIndex.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace algs4net.Collections
+{
+    /// <summary>
+    /// An 'elementary' implementation of <see cref="IIndex{TKey, TValue}"/>
+    /// which uses a Linked List internally, to be used as a basis for
+    /// verifying more advanced implementations.
+    /// </summary>
+    public sealed class SequentialIndex<TKey, TValue> :
+        IIndex<TKey, TValue>
+        where TKey : IComparable<TKey>, IEquatable<TKey>
+    {
+        private readonly LinkedList<IndexEntry> _entries = new LinkedList<IndexEntry>();
+
+        public TValue this[TKey key]
+        {
+            get
+            {
+                if (TryGet(key, out TValue value))
+                {
+                    return value;
+                }
+                throw new Exception($"Key not found.");
+            }
+            set
+            {
+                var entry = new IndexEntry(key, value);
+                _entries.Remove(entry);
+                _entries.InsertBefore(entry, entry);
+            }
+        }
+
+        public IEnumerator<TKey> GetEnumerator()
+        {
+            foreach (var entry in _entries)
+            {
+                yield return entry.Key;
+            }
+        }
+
+        public bool Remove(TKey key)
+        {
+            return TryRemove(key, out TValue value);
+        }
+
+        public bool TryGet(TKey key, out TValue value)
+        {
+            foreach (var entry in _entries)
+            {
+                if ((entry.Key as IEquatable<TKey>).Equals(key))
+                {
+                    value = entry.Value;
+                    return true;
+                }
+            }
+            value = default;
+            return false;
+        }
+
+        public bool TryRemove(TKey key, out TValue value)
+        {
+            return TryGet(key, out value)
+                && _entries.Remove(new IndexEntry(key, value));
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        private class IndexEntry :
+            IComparable<IndexEntry>,
+            IEquatable<IndexEntry>
+        {
+            public readonly TKey Key;
+
+            public TValue Value;
+
+            public IndexEntry(TKey key, TValue value = default)
+            {
+                Key = key;
+                Value = value;
+            }
+
+            public int CompareTo(IndexEntry other)
+            {
+                return Key.CompareTo(other.Key);
+            }
+
+            public bool Equals(IndexEntry other)
+            {
+                return ReferenceEquals(this, other)
+                    || Key.Equals(other.Key);
+            }
+
+            public override string ToString()
+            {
+                return $"{Key}:{Value}";
+            }
+        }
+    }
+}
