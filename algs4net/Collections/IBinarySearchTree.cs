e5706e402291a0b5ec6865ba3651156b2eb5c0b4
wilson0x4d <mrshaunwilson@msn.com>
2018-08-03 18:40:39 -0700

    [collections] add `IBinarySearchTree`

---
+++ algs4net/Collections/IBinarySearchTree.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace algs4net.Collections
+{
+    public interface IBinarySearchTree<TKey, TValue>
+        where TKey : IComparable<TKey>, IEquatable<TKey>
+    {
+        TValue this[TKey key] { get; set; }
+
+        bool TryGetValue(TKey key, out TValue value);
+
+        bool TryRemove(TKey key, out TValue value);
+    }
+}
